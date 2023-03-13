92c7c3500c06d719fd22f9576fa13438ece1d2fc
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2023-03-13 05:20:40 -0300

    style: Clean up main file

--- webservice/app.js
+++ webservice/app.js
@@ -2,13 +2,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const routers = require('./src/routers/router.js');
 
 /* CONFIG */
-const port = 3030;
-
 const app = express();
+const port = 3030;
 
 /* MIDDLEWARE */
 app.use(cors());
@@ -16,7 +14,6 @@ app.use(morgan('dev'));
 app.use(routers);
 
 /* RUN SERVER */
-
 app.listen(port, () => {
 	console.log(`Running application on port:  http://localhost:${port}`);
 });
