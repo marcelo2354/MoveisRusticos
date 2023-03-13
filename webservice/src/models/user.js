2febb13c147fa05f9f946e6189b3e5051b4b77df
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2023-03-13 06:00:08 -0300

    feat: Create new model user

---
+++ webservice/src/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1023,
+    },
+});
+
+userSchema.static('validate', function(user) {
+    const schema = {
+        email: Joi.string().min(5).max(255).email().required(),
+        password: Joi.string().min(5).max(255).required(),
+    }
+    return Joi.validate(user, schema);
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
