c4e5853fb5bdf536e4ef6a2b1017535e4aa53b88
Lucas Ferreira de Almeida <lucasf.almeida.dev@gmail.com>
2023-03-13 05:11:31 -0300

    docs(webservice): Comment main file sections

--- webservice/app.js
+++ webservice/app.js
@@ -1,18 +1,21 @@
-/* eslint-disable linebreak-style */
+/* LIBRARIES */
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
 const routers = require('./src/routers/router.js');
+
+/* CONFIG */
 const port = 3030;
 
 const app = express();
+
+/* MIDDLEWARE */
 app.use(cors());
 app.use(morgan('dev'));
-
-
 app.use(routers);
 
-
+/* RUN SERVER */
 
 app.listen(port, () => {
 	console.log(`Running application on port:  http://localhost:${port}`);
